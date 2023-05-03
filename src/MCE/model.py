02e3a03e80fe68b86fa3239915329d3791f4f409
shalomteresa <shalomteresa4god@gmail.com>
2023-05-03 12:26:36 +0100

    'MCE'

--- src/MCE/model.py
+++ src/MCE/model.py
@@ -16,12 +16,63 @@ import tkinter as tk
 from PIL import Image, ImageTk
 import numpy as np
 
-if __name__ == '__main__' :
-    #read the data from txt file
+def get_weighted_sum(gw, tw, pw, geology, transport, population):
+    """
     
+
+    Parameters
+    ----------
+    gw : TYPE
+        DESCRIPTION.
+    tw : TYPE
+        DESCRIPTION.
+    pw : TYPE
+        DESCRIPTION.
+    geology : TYPE
+        DESCRIPTION.
+    transport : TYPE
+        DESCRIPTION.
+    population : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    weighted_sum : TYPE
+        DESCRIPTION.
+
+    >>> gw = 0.3
+    >>> tw = 0.4
+    >>> pw = 0.3
+    >>> geology = [[1], [2]]
+    >>> transport = [[0], [1]]
+    >>> population = [[3], [0]]
+    >>> ws = get_weighted_sum(gw, tw, pw, geology, transport, population)
+    >>> print(ws)
+    [[1.2], [1.0]]
+    """
+    # Calculate the weighted sum
+    weighted_sum = []
+    for i in range(len(population)):
+        row = []
+        for j in range(len(population[0])):
+            # Calculate the weighted sum for the current pixel
+            row.append(geology[i][j] * gw + population[i][j] * pw + transport[i][j] * tw)
+        weighted_sum.append(row)
+    return weighted_sum
+    
+    
+    
+if __name__ == '__main__' :
+    
+    # For testing
+    import doctest
+    doctest.testmod()
+    
+    #read the data from txt file
+        
     geology, n_rows, n_cols = io.read_data('../../data/input/geology.txt')
     
-    print(geology)
+    #print(geology)
     plt.imshow(geology)
     plt.show()
     
@@ -34,68 +85,53 @@ if __name__ == '__main__' :
     plt.show()
     
     
-    # Defining the weights 
+    # Define the weights
     gw = 0.3
     pw = 0.3
     tw = 0.4
     
-    result = []
+    weighted_sum = get_weighted_sum(gw, tw, pw, geology, transport, population)
+    # # Calculate the weighted sum
+    # weighted_sum = []
+    # for i in range(n_rows):
+    #     row = []
+    #     for j in range(n_cols):
+    #         # Calculate the weighted sum for the current pixel
+    #         row.append(geology[i][j] * gw + population[i][j] * pw + transport[i][j] * tw)
+    #     weighted_sum.append(row)
+    
+    # Find the maximum and minimum values in combined
+    max_val = weighted_sum[0][0]
+    min_val = max_val
     for i in range(n_rows):
         for j in range(n_cols):
-            weighted_sum = geology[i][j]*gw + population[i][j]*pw + transport[i][j]*tw 
-            result.append(weighted_sum)
+            max_val = max(max_val, weighted_sum[i][j])
+            min_val = min(min_val, weighted_sum[i][j])
             
-               
-    print(result)
-    plt.imshow(result)
-    plt.show()
+    print(min_val)
+    print(max_val)
+    #print(weighted_sum)
     
-    # Rescale the result to be between 0 and 255
-    max_val = max(result)
-    min_val = min(result)
-    scaled_result = [(val - min_val) / (max_val - min_val) * 255 for val in result]
-
-                
-    # Reshape the scaled_result list into a 2D array with the same shape as the original data
-    scaled_data = np.array(scaled_result).reshape((n_rows, n_cols))
-    
-    # Display the image
-    plt.imshow(scaled_data, cmap='gray')
-    plt.show()
-    
-    
-    # # Read in the geology, population, and transport rasters
-    # data = io.read_data(['../../data/input/geology.txt', '../../data/input/population.txt', '../../data/input/transport.txt'])
-    # weights = [0.3, 0.4, 0.3]
-    # weighted_rasters = geometry.weighted_rasters(data, weights)
-    # #print(weighted_rasters)
-    
-    # weighted_sum = geometry.add_rasters(weighted_rasters)
-    # #print(weighted_sum)
+    # Calculate rescaled output
+    rescaled_output = []
+    for i in range(n_rows):
+        row = []
+        for j in range(n_cols):
+            rescaled_value = int((weighted_sum[i][j] - min_val) / (max_val - min_val) * 255)
+            #if rescaled_value != 0:
+            #    print(rescaled_value)
+            # Add the rescaled value to the current row
+            row.append(rescaled_value)
+        # Add the current row to the output list
+        rescaled_output.append(row)
         
-    # rescaled_raster = geometry.rescale_raster(weighted_sum)
-    # #print(rescaled_raster)
     
-    # # Create directory to write images to.
-    # try:
-    #     os.makedirs('../../data/output/images/')
-    # except FileExistsError:
-    #     print("path exists")
+    # Display the rescaled output as an image
+    plt.imshow(rescaled_output)
+    plt.show()
     
-    # # For storing images
-    # global ite
-    # ite = 1
-    # images = []
     
-    # output = io.write_Data(rescaled_raster)
+        
 
-    
-    
-    # plt.imshow(rescaled_raster)
-    # filename = '../../data/output/images/image' + str(ite) + '.png'
-    # plt.savefig(filename)
-    # plt.show()
-    # plt.close()
-    # images.append(imageio.imread(filename))
 
    
